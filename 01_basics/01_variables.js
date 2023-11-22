5cc9be8f0562e2ebcb42b8a85bd434bf7a4f2657
Akshat Gaur <113232451+Gaur05@users.noreply.github.com>
2023-11-22 18:41:58 +0000

    01_Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 3049
+let accountEmail = "akshatgaur02@gmail"
+var accountPassword = "12345"
+accountCity = "Bengaluru"
+let accountState
+
+// accountID = 8569 // not allowed to change the const value
+// console.log(accountID);
+
+/*
+Prefer not to use var
+beacause of issue in block scope and functional scope 
+*/
+console.table([accountID, accountEmail, accountPassword,accountCity,accountState])
