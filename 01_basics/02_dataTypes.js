be2d9e821a10ea59dcea421da239624b9c14226b
Akshat Gaur <113232451+Gaur05@users.noreply.github.com>
2023-11-22 19:08:13 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) //we are using node js, not browser
+
+console.log(3
+    +3)// code readability should be high
+
+console.log(3+3) // this code must be writter in this manner
+
+
+console.log("Gaur")
+
+// Documentation links:
+// https://tc39.es/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+
+let name = "Gaur"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object
