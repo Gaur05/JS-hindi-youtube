ef628143c65d91a264614c83cf809829a787d314
Akshat Gaur <113232451+Gaur05@users.noreply.github.com>
2024-01-18 07:06:42 +0530

    Update 04_Objects.js

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -69,7 +69,7 @@ const {courseInstructor: instructor} = course
 console.log(instructor);
 
 // {
-//     "name": "hitesh",
+//     "name": "Gaur",
 //     "coursename": "js in hindi",
 //     "price": "free"
 // }
